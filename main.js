dea3b4ccbac1f52af97c012575aa4b990c898aba
Evgeniy.Barankov <mdjek@mail.ru>
2021-12-10 23:11:04 +0300

    added hp damage

--- main.js
+++ main.js
@@ -7,12 +7,20 @@ const NAME_MAP = {
 };
 
 const arenasBlock = document.querySelector('.arenas');
+const randomButton = document.querySelector('.control button');
 
-function Player (name, hp, weapon) {
+const between = (min = 1, max = 20) => {
+  return Math.floor(
+    Math.random() * (max - min + 1) + min
+  );
+};
+
+function Player (name, weapon, position, hp = 100) {
   this.name = name;
+  this.weapon = weapon;
+  this.player = position;
   this.hp = hp;
   this.img = NAME_MAP[name];
-  this.weapon = weapon;
   this.attack = function () { console.log(`${this.name} Fight...`) };
 }
 
@@ -28,13 +36,13 @@ const createHTMLElement = (tag = 'div', classname, content) => {
   }
 
   if (typeof content === 'object' && content.length && content.length > 0) {
-     content.forEach((item) => element.appendChild(item));
+    content.forEach((item) => element.appendChild(item));
   }
 
   return element;
 }
 
-const createPlayerMarkup = (playerName, name, hp = 100, pathToImg) => {
+const createPlayerMarkup = (playerName, name, hp, pathToImg) => {
   const lifeEl = createHTMLElement('div', 'life');
   const nameEl = createHTMLElement('div', 'name', name);
   const imgEl = createHTMLElement('img');
@@ -55,8 +63,34 @@ const createPlayer = (playerName, data) => {
   arenasBlock.appendChild(player);
 }
 
-const player1 = new Player('SCORPION', 45, ['example', 'gun']);
-const player2 = new Player('SONYA', 75, ['example', 'gun']);
+const changeHP = (player, opponent) => {
+  const playerLife = document.querySelector(`.player${player.player} .life`);
+
+  player.hp -= between();
+
+  if (player.hp <= 0) {
+    player.hp = 0;
+    randomButton.disabled = true;
+
+    renderPlayerWin(opponent.name);
+  }
+
+  playerLife.style.width = `${player.hp}%`;
+}
+
+const renderPlayerWin = (name) => {
+  const winTitle = createHTMLElement('div', 'winTitle', `${name} wins`);
+
+  arenasBlock.appendChild(winTitle);
+}
+
+const player1 = new Player('SCORPION', ['example', 'gun'], 1);
+const player2 = new Player('SONYA', ['example', 'gun'], 2);
+
+randomButton.addEventListener('click', () => {
+  changeHP(player1, player2);
+  changeHP(player2, player1);
+});
 
 createPlayer('player1', player1);
 createPlayer('player2', player2);

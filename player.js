0d9045f673b4638c335e7aa73d2704cd912a88e0
Evgeniy.Barankov <mdjek@mail.ru>
2021-12-22 13:31:37 +0300

    creation player with class, additional destructuring

--- player.js
+++ player.js
@@ -1,39 +1,36 @@
 import { NAME_MAP } from './constants.js';
 
-function changeHP(value) {
-  this.hp -= value;
-
-  if (this.hp < 0) {
-    this.hp = 0;
+class Player {
+  constructor(props) {
+    this.id = props.id;
+    this.name = props.name;
+    this.hp = props.hp;
+    this.img = props.img;
   }
+
+  changeHP = (value) => {
+    this.hp -= value;
+
+    if (this.hp < 0) {
+      this.hp = 0;
+    }
+  };
+
+  elHP = () => document.querySelector(`.player${this.id} .life`);
+
+  renderHP = () => this.elHP().style.width = `${this.hp}%`;
 }
 
-function elHP() {
-  return document.querySelector(`.player${this.player} .life`);
-}
-
-function renderHP() {
-  this.elHP().style.width = `${this.hp}%`;
-}
-
-export const player1 = {
+export const player1 = new Player({
+  id: 1,
   name: 'SCORPION',
   hp: 100,
-  player: 1,
   img: NAME_MAP['SCORPION'],
-  weapon: ['example', 'gun'],
-  changeHP,
-  elHP,
-  renderHP
-};
+});
 
-export const player2 = {
+export const player2 = new Player({
+  id: 2,
   name: 'SONYA',
   hp: 100,
-  player: 2,
   img: NAME_MAP['SONYA'],
-  weapon: ['example', 'gun'],
-  changeHP,
-  elHP,
-  renderHP
-};
+});
